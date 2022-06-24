37f7020b64e75b751c885871d92e6a2ef7857724
Vinayak <vinayak20029@gmail.com>
2022-06-24 21:44:02 +0530

    test

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use("/api", require("./routes"));
-app.use("/test", (req, res) => {
+app.get("/test", (req, res) => {
   return res.send("its working");
 });
 // app.use("/", express.static("./public"));
